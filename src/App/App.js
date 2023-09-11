d78f22343008549479cc35602c07014ee3b4b76f
Avery Berryman <averyberryman@gmail.com>
2023-09-11 11:47:36 -0600

    Add working form

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import { getReservations } from '../APICalls';
 import Reservations from '../Reservations/reservations';
+import ReservationForm from '../ReservationForm/reservationForm';
 
 
 function App() {
@@ -17,14 +18,19 @@ function App() {
       });
   }, []);
 
+  function handleAddReservation(newReservation) {
+    setReservations(prevReservations => [...prevReservations, newReservation]);
+}
+
   return (
     <div className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
+      <ReservationForm addReservation={handleAddReservation} />
       <div className='resy-form'>
       </div>
       <Reservations reservations={reservations} />
     </div>
-  );
+ );
 }
 
 export default App;
